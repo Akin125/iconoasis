77c5b0b857090bcf1a33205b859d1e9a8464cd51
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-05-20 23:20:39 +0100

    done with search filter and icon compo

--- src/component/SearchFilter/SearchFilter.js
+++ src/component/SearchFilter/SearchFilter.js
@@ -1,7 +1,15 @@
 import React from "react";
+import IconComponent from "../IconComponent/IconComponent";
+import {Icons} from "./searchicondata";
 
-export default  function SearchFilter (){
+
+export default  function SearchFilter () {
+
+    const [query, setQuery] = React.useState('')
     const [dropDown, setDropdown] = React.useState(false)
+    const [selectItem, setSelectItem] = React.useState('PNG');
+    // const icons = Object.values(jsonData.icon);
+
 
     function handleDropdown(){
         if(!dropDown){
@@ -11,34 +19,65 @@ export default  function SearchFilter (){
         }
     }
 
+    function handleSelection(x){
+        setSelectItem(x);
+        setDropdown(false)
+    }
+
     return(
-        <div className="center">
+            <div className="center">
 
 
-        <div className='filterBody'>
-            <div className="selectFormat" >
-                <div className="format" onClick={handleDropdown}>PNG
-                    <span className='dropdown' >
-                        <img src="https://res.cloudinary.com/do5wu6ikf/image/upload/v1684572720/alx/Down_arrow_x9x9ni.svg" alt=""/>
-                    </span>
+            <div className='filterBody'>
+                <div className="selectFormat" >
+
+                    <div className="format" onClick={handleDropdown}>{selectItem}
+                        <span className='dropdown' >
+                            <img src="https://res.cloudinary.com/do5wu6ikf/image/upload/v1684572720/alx/Down_arrow_x9x9ni.svg" alt=""/>
+                        </span>
+                    </div>
+
+                    {dropDown &&  <div className="formatDropdown">
+                        <ul>
+                            <li onClick={() => handleSelection('PNG')}>PNG</li>
+                            <li className='line' onClick={() => handleSelection('JPEG')}>JPEG</li>
+                            <li className='line' onClick={() => handleSelection('AI')}>AI</li>
+                            <li className='line' onClick={() => handleSelection('SVG')}>SVG</li>
+                        </ul>
+                    </div>}
+
                 </div>
 
-                {dropDown &&  <div className="formatDropdown">
-                    <ul>
-                        <li>PNG</li>
-                        <li className='line'>JPEG</li>
-                        <li className='line'>AI</li>
-                        <li className='line'>SVG</li>
-                    </ul>
-                </div>}
-                
-            </div>
-            
-            <div className="searchInput">
-                <input type="text" placeholder='Search you Icons.....'/>
+                <div className="searchInput">
+                    <input type="text"
+                      placeholder='Search you Icons.....'
+                      onChange={(event) => setQuery(event.target.value)}
+                    />
+                    {/*<ul className="searchlist">*/}
+                    {/*    {Icons.filter((icon) => icon.name.toLowerCase().includes(query)*/}
+                    {/*    ).map((icon) => (*/}
+                    {/*        <li key={icon.id}>{icon.name}</li>*/}
+                    {/*    ))}*/}
+                    {/*</ul>*/}
+
+                </div>
+
+
+                {/*<p>{searchicondata.icon[0].format}</p>*/}
             </div>
 
-        </div>
-        </div>
-    )
-}
+                <table>
+                    <tbody>
+                    {Icons.filter((icon) => icon.name.toLowerCase().includes(query)
+                    ).map((icon) => ( <td key={icon.id}>
+                            <IconComponent
+                                img={icon.link}
+                                name={icon.name}
+                            />
+                    </td>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+    )}
+
---
+++ src/component/SearchFilter/searchicondata.js
@@ -0,0 +1,149 @@
+export const Icons = [
+    {
+      "id": "1",
+      "name": "Chair",
+      "link": "https://cdn-icons-png.flaticon.com/128/10786/10786671.png",
+      "format": "png",
+      "searchFilter": [ "furniture", "chair" ]
+    },
+    {
+      "id": "2",
+      "name": "Wifi",
+      "link": "https://cdn-icons-png.flaticon.com/128/10786/10786652.png",
+      "format": "png",
+      "searchFilter": [
+        "Wifi",
+        "internet"
+      ]
+    },
+    {
+      "id": "3",
+      "name": "Calendar",
+      "link": "https://cdn-icons-png.flaticon.com/128/10786/10786674.png",
+      "format": "png",
+      "searchFilter": [
+        "calender date",
+        "when"
+      ]
+    },
+    {
+      "id": "4",
+      "name": "Bike",
+      "link": "https://cdn-icons-png.flaticon.com/128/10786/10786749.png",
+      "format": "png",
+      "searchFilter": [
+        "cycle",
+        "time"
+      ]
+    },
+    {
+      "id": "5",
+      "name": "Contacts",
+      "link": "https://example.com/icons/contacts.svg",
+      "format": "svg",
+      "searchFilter": [
+        "people",
+        "friends"
+      ]
+    },
+    {
+      "id": "6",
+      "name": "Camera",
+      "link": "https://example.com/icons/camera.png",
+      "format": "png",
+      "searchFilter": [
+        "photo",
+        "video"
+      ]
+    },
+    {
+      "id": "7",
+      "name": "Microphone",
+      "link": "https://example.com/icons/microphone.svg",
+      "format": "svg",
+      "searchFilter": [
+        "voice",
+        "recording"
+      ]
+    },
+    {
+      "id": "8",
+      "name": "Speaker",
+      "link": "https://example.com/icons/speaker.png",
+      "format": "png",
+      "searchFilter": [
+        "sound",
+        "music"
+      ]
+    },
+    {
+      "id": "9",
+      "name": "Clock",
+      "link": "https://example.com/icons/clock.svg",
+      "format": "svg",
+      "searchFilter": [
+        "time",
+        "alarm"
+      ]
+    },
+    {
+      "id": "10",
+      "name": "Calculator",
+      "link": "https://example.com/icons/calculator.png",
+      "format": "png",
+      "searchFilter": [
+        "math",
+        "numbers"
+      ]
+    },
+    {
+      "id": "11",
+      "name": "Browser",
+      "link": "https://example.com/icons/browser.svg",
+      "format": "svg",
+      "searchFilter": [
+        "internet",
+        "web"
+      ]
+    },
+    {
+      "id": "12",
+      "name": "File Manager",
+      "link": "https://example.com/icons/file-manager.png",
+      "format": "png",
+      "searchFilter": [
+        "files",
+        "folders"
+      ]
+    },
+    {
+      "id": "13",
+      "name": "Notes",
+      "link": "https://example.com/icons/notes.svg",
+      "format": "svg",
+      "searchFilter": [
+        "writing",
+        "to-do"
+      ]
+    },
+    {
+      "id": "14",
+      "name": "Weather",
+      "link": "https://example.com/icons/weather.png",
+      "format": "png",
+      "searchFilter": [
+        "temperature",
+        "forecast"
+      ]
+    },
+    {
+      "id": "15",
+      "name": "News",
+      "link": "https://example.com/icons/news.svg",
+      "format": "svg",
+      "searchFilter": [
+        "current events",
+        "politics"
+      ]
+    }
+  ]
