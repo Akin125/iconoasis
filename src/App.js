a43079078ed0fefe1e1b6bccd1610da58e07f8ff
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-05-20 11:00:45 +0100

    first commit on stallion branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "./component/NavBar/Navbar";
 import './App.css';
+import SearchFilter from "./component/SearchFilter/SearchFilter";
 
 function App() {
   return (
       <>
-        <NavBar/>
+            <NavBar/>
+            <SearchFilter/>
       </>
   );
 }
--- src/component/SearchFilter/SearchFilter.js
+++ src/component/SearchFilter/SearchFilter.js
@@ -1,8 +1,39 @@
 import React from "react";
 
 export default  function SearchFilter (){
+    const [dropDown, setDropdown] = React.useState(false)
+
+    function handleDropdown(){
+        if(!dropDown){
+        setDropdown(true)
+        }else {
+            setDropdown(false)
+        }
+    }
+
     return(
         <div className='filterBody'>
+            <div className="selectFormat" onClick={handleDropdown}>
+                <div className="format">PNG
+                    <span className='dropdown' >
+                        <img src="https://res.cloudinary.com/do5wu6ikf/image/upload/v1684572720/alx/Down_arrow_x9x9ni.svg" alt=""/>
+                    </span>
+                </div>
+
+                {dropDown &&  <div className="formatDropdown">
+                    <ul>
+                        <li>PNG</li>
+                        <li className='line'>JPEG</li>
+                        <li className='line'>AI</li>
+                        <li className='line'>SVG</li>
+                    </ul>
+                </div>}
+                
+            </div>
+            
+            <div className="searchInput">
+                <input type="text" placeholder='Search you Icons.....'/>
+            </div>
 
         </div>
     )
