ea72237b0d743daf364e00436bd6f5e7917c9813
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-05-21 08:43:49 +0100

    done with color i know the colors are not that good

--- src/component/NavBar/Navbar.js
+++ src/component/NavBar/Navbar.js
@@ -4,6 +4,10 @@ export default function NavBar(){
     return(
         <div className='mainNav'>
             <div className="nav">ICONOASIS</div>
+
+            <div className="user">
+                UserName<span>Sign out</span>
+            </div>
         </div>
     )
 }
